6ea6fc70f349c861445809c8c2dadaa8fa3cbb97
Patrick Connolly <patmconnolly@gmail.com>
2025-07-26 19:32:03 -0500

    Run slapd outside the entrypoint command.

--- OpenLDAP/entrypoint.sh
+++ OpenLDAP/entrypoint.sh
@@ -6,5 +6,3 @@ ROOTPW="$3"
 sed -i "/^suffix[[:space:]]*\"dc=my-domain,dc=com\"$/c\suffix\t\t\"${SUFFIX}\"" /usr/local/etc/openldap/slapd.conf
 sed -i "/^rootdn[[:space:]]*\"cn=Manager,dc=my-domain,dc=com\"$/c\rootdn\t\t\"${ROOTDN}\"" /usr/local/etc/openldap/slapd.conf
 sed -i "/^rootpw[[:space:]]*secret$/c\rootpw\t\t${ROOTPW}" /usr/local/etc/openldap/slapd.conf
-
-/usr/local/libexec/slapd
