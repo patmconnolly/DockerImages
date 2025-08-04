662461e0c907f1356ce20f33b1286d4a3ac0e022
Patrick Connolly <patmconnolly@gmail.com>
2025-08-04 16:55:07 -0500

    Fix first run flag set.

--- OpenLDAP/entrypoint.sh
+++ OpenLDAP/entrypoint.sh
@@ -15,9 +15,9 @@ sed -i "/^olcRootDN:[[:space:]]*cn=Manager,dc=my-domain,dc=com$/c\olcRootDN: ${R
 sed -i "/^olcRootPW:[[:space:]]*secret$/c\olcRootPW: ${ROOTPW}" /usr/local/etc/openldap/slapd.ldif
 
 if [ ! -f "$FIRST_RUN_FLAG_FILE" ]; then
-	echo "This is the first run, creating flag file."
-	echo '$FIRST_RUN_FLAG_FILE_DATA' > '$FIRST_RUN_FLAG_FILE'
-	/usr/local/sbin/slapadd -n 0 -F /usr/local/etc/slapd.d -l /usr/local/etc/openldap/slapd.ldif
+        echo "This is the first run, creating flag file."
+        echo "${FIRST_RUN_FLAG_FILE_DATA}" > "${FIRST_RUN_FLAG_FILE}"
+        /usr/local/sbin/slapadd -n 0 -F /usr/local/etc/slapd.d -l /usr/local/etc/openldap/slapd.ldif
 else
-	echo "This is not the first run, flag file detected."
+        echo "This is not the first run, flag file detected."
 fi
